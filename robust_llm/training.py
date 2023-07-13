fd05338ef51afa66c0c5e9148822c4aab46bb9de
Niki Howe <nikihowe@gmail.com>
2023-07-12 21:39:10 -0400

    Try to print incorrect predictions

--- robust_llm/training.py
+++ robust_llm/training.py
@@ -3,7 +3,37 @@ import evaluate
 import numpy as np
 
 from datasets import Dataset
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, TrainerCallback
+
+
+class PrintIncorrectClassificationsCallback(TrainerCallback):
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        print("Printing incorrect eval classifications (or 20 of them if there are too many):")
+        model = kwargs["model"]
+        eval_dataloader = kwargs["eval_dataloader"]
+
+        # Get the incorrect predictions
+        incorrect_predictions = []
+        batch = eval_dataloader.dataset  # we do this full batch for simplicity
+        batch = {k: v.to(args.device) for k, v in batch.items()}
+        outputs = model(**batch)
+        logits = outputs.logits
+        predictions = np.argmax(logits, axis=-1)
+        incorrect_predictions.append(predictions != batch["labels"])
+
+        # Print up to 20 of them
+        incorrect_predictions = np.concatenate(incorrect_predictions)
+        if incorrect_predictions.sum() > 20:
+            print("Too many incorrect predictions to print. Printing the first 20.")
+            incorrect_predictions = incorrect_predictions[:20]
+        for i, is_incorrect in enumerate(incorrect_predictions):
+            if is_incorrect:
+                print(f"Example {i}:")
+                print(f"Input: {batch['input_ids'][i]}")
+                print(f"Label: {batch['labels'][i]}")
+                print(f"Prediction: {predictions[i]}")
+                print()
 
 
 @dataclasses.dataclass
@@ -19,7 +49,7 @@ class Training:
     def run_trainer(self):
         hf_training_args = TrainingArguments(
             output_dir="test_trainer",
-            num_train_epochs=3,
+            num_train_epochs=1,
             eval_steps=32,
             evaluation_strategy="steps",
             logging_steps=1,
