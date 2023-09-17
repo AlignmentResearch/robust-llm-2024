467fb1179cced10ba344db5c5e5f62852c133a5f
Niki Howe <nikihowe@gmail.com>
2023-09-17 01:33:12 +0000

    Disentangle two meanings of "max length"

--- robust_llm/language_generators/__init__.py
+++ robust_llm/language_generators/__init__.py
@@ -6,13 +6,13 @@ from robust_llm.language_generators.tomita7 import Tomita7
 
 def make_language_generator_from_args(args):
     if args.language_generator == "Tomita1":
-        language_generator = Tomita1(args.max_length)
+        language_generator = Tomita1(max_length=args.max_length)
     elif args.language_generator == "Tomita2":
-        language_generator = Tomita2(args.max_length)
+        language_generator = Tomita2(max_length=args.max_length)
     elif args.language_generator == "Tomita4":
-        language_generator = Tomita4(args.max_length)
+        language_generator = Tomita4(max_length=args.max_length)
     elif args.language_generator == "Tomita7":
-        language_generator = Tomita7(args.max_length)
+        language_generator = Tomita7(max_length=args.max_length)
     else:
         raise ValueError(f"Unknown language generator: {args.language_generator}")
 
