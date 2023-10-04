b34c4ebfa1ee5d802a0aabe4b4553f5c1b43e892
Dan Pandori <dantweinand@gmail.com>
2023-10-04 16:41:57 -0700

    Delete unused.

--- robust_llm/utils.py
+++ robust_llm/utils.py
@@ -14,20 +14,6 @@ from typing import Generator
 from datasets import Dataset
 from transformers import Trainer
 
-
-def check_input_length(input_text, tokenizer):
-    max_length = tokenizer.max_length
-
-    inputs = tokenizer.encode(input_text, truncation=True, padding=False)
-    input_length = len(inputs)
-
-    if input_length > max_length:
-        print(
-            f"Warning: Input length ({input_length}) exceeds the maximum model length ({max_length})."
-        )
-        raise ValueError
-
-
 def tokenize_dataset(dataset, tokenizer):
     # Padding seems necessary in order to avoid an error
     tokenized_data = tokenizer(dataset["text"], padding="max_length", truncation=True)
