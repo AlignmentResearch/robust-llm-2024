f17a3fca937f78bb1aa2020954e4d4ab264a8483
Dan Pandori <dantweinand@gmail.com>
2023-10-04 14:22:26 -0700

    Exception

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -35,7 +35,7 @@ def test_basic_constructor():
             eval_dataset=train_dataset,
             model=model,
         )
-    except e:
+    except Exception as e:
         logger.warn(f"Failed to initialize Training. {e}")
 
 
