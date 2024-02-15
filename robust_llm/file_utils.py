77e77f3b1912493ec32cbf33fcf694c25562bb9d
Niki Howe <13190901+nikihowe@users.noreply.github.com>
2024-02-15 16:06:02 +0100

    Avoid mypy datasets error (#186)

--- robust_llm/file_utils.py
+++ robust_llm/file_utils.py
@@ -8,7 +8,7 @@ def compute_repo_path() -> str:
 
 def compute_dataset_path() -> str:
     path_to_repo = compute_repo_path()
-    return f"{path_to_repo}/robust_llm/datasets"
+    return f"{path_to_repo}/robust_llm/local_datasets"
 
 
 def compute_dataset_management_path() -> str:
