cf45e75975ba208e8cadbddf3caa67d75ca9250d
Niki Howe <13190901+nikihowe@users.noreply.github.com>
2024-04-01 16:49:17 -0400

    Train 6.9b and 12b models on word length task (#250)

---
+++ experiments/niki/niki_015_word_length_ft_12b.py
@@ -0,0 +1,32 @@
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+HYDRA_CONFIG = "simple_train_word_length"
+
+PYTHIA_NAMES_AND_FINETUNED_TARGET_NAMES = [
+    ("EleutherAI/pythia-12b-deduped", "pythia-word-length-12b-niki-ada-v1"),
+]
+OVERRIDE_ARGS_LIST = [
+    {
+        "experiment.environment.model_name_or_path": base_model_name,
+        "experiment.training.force_name_to_save": target_model_name,
+        "experiment.training.optimizer": "adafactor",
+        "experiment.training.batch_size": 2,
+        "experiment.evaluation.batch_size": 16,
+        # Disable saving during training, we were getting some crashes here.
+        "experiment.training.save_strategy": "no",
+    }
+    for (base_model_name, target_model_name) in PYTHIA_NAMES_AND_FINETUNED_TARGET_NAMES
+]
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        gpu=4,
+        use_accelerate=True,
+        memory="220G",
+    )
