9c8fcab1cc207443e0eea6a71c49579e425c8246
Niki Howe <13190901+nikihowe@users.noreply.github.com>
2024-04-04 15:12:31 -0400

    Add GPU memory tracking code (#269)

---
+++ robust_llm/debug_utils.py
@@ -0,0 +1,30 @@
+# Inspired by https://gist.github.com/s-mawjee/ad0d8e0c7e07265cae097899fe48c023
+
+import nvidia_smi
+
+
+def _check_gpu():
+    nvidia_smi.nvmlInit()
+    num_gpu = nvidia_smi.nvmlDeviceGetCount()
+    gpu = num_gpu > 0
+
+    return gpu, num_gpu
+
+
+def _bytes_to_megabytes(bytes):
+    return round((bytes / 1024) / 1024, 2)
+
+
+def print_gpu_usage():
+    gpu, num_gpu = _check_gpu()
+    if not gpu:
+        print("No GPU found, so no usage to report.")
+        return
+
+    for i in range(num_gpu):
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+        info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+        print(
+            f"GPU-{i}: GPU-Memory: "
+            f"{_bytes_to_megabytes(info.used)}/{_bytes_to_megabytes(info.total)} MB"
+        )
