859b083905d5912cd58c48ff1bd54aa161d43ff5
Michał Zając <emzajac@gmail.com>
2024-05-01 20:28:40 +0200

    Michal: recent adversarial training experiments (#360)
    Co-authored-by: Ian McKenzie <ianmck98@gmail.com>

---
+++ experiments/mz/mz_131f_adv_training_long.py
@@ -0,0 +1,57 @@
+"""Look at longer adversarial training (30 rounds).
+We add 200 adversarial examples each round.
+"""
+
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+# get just the <name>_<exp_number> part with a hyphen instead of underscore
+EXP_PREFIX = "-".join(EXPERIMENT_NAME.split("_")[:2])
+HYDRA_CONFIG = "20240423_mz_131"
+
+MODELS_AND_N_MAX_PARALLEL = [
+    ("pythia-14m", 2),
+    ("pythia-31m", 2),
+    ("pythia-70m", 2),
+    ("pythia-160m", 2),
+    ("pythia-410m", 1),
+    ("pythia-1b", 1),
+]
+DATASETS = ["IMDB", "PasswordMatch"]
+APPEND_TO_MODIFIABLE_CHUNK = {
+    "IMDB": True,
+    "PasswordMatch": False,
+}
+
+OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL = [
+    (
+        {
+            "experiment.environment.model_name_or_path": f"EleutherAI/{model}",
+            "experiment.training.force_name_to_save": f"{model}_{EXP_PREFIX}_{dataset}",  # noqa: E501
+            "experiment.dataset.dataset_type": f"AlignmentResearch/{dataset}",
+            "experiment.training.iterative.training_attack.append_to_modifiable_chunk": APPEND_TO_MODIFIABLE_CHUNK[  # noqa: E501
+                dataset
+            ],
+            "experiment.evaluation.evaluation_attack.append_to_modifiable_chunk": APPEND_TO_MODIFIABLE_CHUNK[  # noqa: E501
+                dataset
+            ],
+        },
+        n_max_parallel,
+    )
+    for (model, n_max_parallel) in MODELS_AND_N_MAX_PARALLEL
+    for dataset in DATASETS
+]
+OVERRIDE_ARGS_LIST = [x[0] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+N_MAX_PARALLEL = [x[1] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        N_MAX_PARALLEL,
+        memory="60G",
+        priority="normal-batch",
+    )
---
+++ experiments/mz/mz_134_adv_training_imdb_14m_hp_search.py
@@ -0,0 +1,46 @@
+"""In mz_131 we saw that for 14M model, attack success rate plateaus at around 10%,
+which is high. See if we can improve adv training by varying hyperparameters.
+"""
+
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+# get just the <name>_<exp_number> part with a hyphen instead of underscore
+EXP_PREFIX = "-".join(EXPERIMENT_NAME.split("_")[:2])
+HYDRA_CONFIG = "20240429_mz_134"
+
+MODEL = "pythia-14m"
+DATASET = "IMDB"
+NUM_GENERATED_EXAMPLES = [100, 200, 500]
+LR = [3e-6, 1e-5, 3e-5]
+
+OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL = [
+    (
+        {
+            "experiment.training.force_name_to_save": f"{MODEL}_{EXP_PREFIX}_{DATASET}",  # noqa: E501
+            "experiment.training.iterative.num_examples_to_generate_each_round": num_generated_examples,  # noqa: E501
+            "experiment.training.iterative.only_add_successful_adversarial_examples": only_successful,  # noqa: E501
+            "experiment.training.iterative.use_balanced_sampling": balanced,
+            "experiment.training.learning_rate": lr,
+        },
+        2,  # n_max_parallel
+    )
+    for num_generated_examples in NUM_GENERATED_EXAMPLES
+    for only_successful in [False, True]
+    for balanced in [False, True]
+    for lr in LR
+]
+OVERRIDE_ARGS_LIST = [x[0] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+N_MAX_PARALLEL = [x[1] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        N_MAX_PARALLEL,
+        memory="60G",
+        priority="normal-batch",
+    )
