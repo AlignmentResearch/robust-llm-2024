40633e6f4a4bccb6ea8c6bb51cc3ad2317bbc7d5
Ian McKenzie <ianmck98@gmail.com>
2024-06-24 12:05:30 -0700

    Initial Qwen experiments (#502)

---
+++ experiments/ian/ian_042_gen_pm_gcg_weaker_qwen.py
@@ -0,0 +1,39 @@
+# Similar to 039 but with bug fixes and a different config setup.
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+HYDRA_CONFIG = "ian/042_gen_pm_gcg_weaker_qwen"
+
+MODELS_AND_N_MAX_PARALLEL = [
+    ("Qwen/Qwen1.5-0.5B-Chat", 2),
+    ("Qwen/Qwen1.5-1.8B-Chat", 2),
+    ("Qwen/Qwen1.5-4B-Chat", 1),
+    ("Qwen/Qwen1.5-7B-Chat", 1),
+]
+
+
+OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL = [
+    (
+        {
+            "+model": model,
+        },
+        n_max_parallel,
+    )
+    for (model, n_max_parallel) in MODELS_AND_N_MAX_PARALLEL
+]
+
+OVERRIDE_ARGS_LIST = [x[0] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+N_MAX_PARALLEL = [x[1] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        N_MAX_PARALLEL,
+        memory="60G",
+        cpu=8,
+        priority="normal-batch",
+    )
--- robust_llm/models/__init__.py
+++ robust_llm/models/__init__.py
@@ -2,6 +2,7 @@ from .supported_models.gpt2_wrapped import GPT2Model
 from .supported_models.gpt_neox_wrapped import GPTNeoXModel
 from .supported_models.llama2_wrapped import Llama2Model
 from .supported_models.qwen_chat_wrapped import QwenChatModel
+from .supported_models.qwen_wrapped import QwenModel
 from .supported_models.tinyllama_chat_wrapped import TinyLlamaChatModel
 from .wrapped_model import WrappedModel
 
@@ -11,5 +12,6 @@ __all__ = [
     "GPT2Model",
     "Llama2Model",
     "QwenChatModel",
+    "QwenModel",
     "TinyLlamaChatModel",
 ]
