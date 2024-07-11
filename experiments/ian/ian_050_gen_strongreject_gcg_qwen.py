c18636bf5e8610cfd7bec6670d73a0211852af20
Ian McKenzie <ianmck98@gmail.com>
2024-07-10 17:46:28 -0700

    Add more StrongREJECT experiments (#605)

---
+++ experiments/ian/ian_050_gen_strongreject_gcg_qwen.py
@@ -0,0 +1,40 @@
+# Same as 049 but with all the bugs I could find fixed.
+# (Also with the naming fixed; it's not a PM experiment.)
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+HYDRA_CONFIG = "ian/050_gen_strongreject_gcg_qwen"
+
+MODELS_AND_N_MAX_PARALLEL = [
+    ("Qwen/Qwen1.5-0.5B-Chat", 1),
+    ("Qwen/Qwen1.5-1.8B-Chat", 1),
+    ("Qwen/Qwen1.5-4B-Chat", 1),
+    ("Qwen/Qwen1.5-7B-Chat", 1),
+    ("Qwen/Qwen1.5-14B-Chat", 1),
+]
+
+OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL = [
+    (
+        {
+            "+model": model,
+        },
+        n_max_parallel,
+    )
+    for (model, n_max_parallel) in MODELS_AND_N_MAX_PARALLEL
+]
+
+OVERRIDE_ARGS_LIST = [x[0] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+N_MAX_PARALLEL = [x[1] for x in OVERRIDE_ARGS_LIST_AND_N_MAX_PARALLEL]
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        N_MAX_PARALLEL,
+        memory="60G",
+        cpu=8,
+        priority="normal-batch",
+    )
