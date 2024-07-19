e1b6e83b30523ba55369190b9ed746f72656e54c
Oskar John Hollinsworth <67026888+ojh31@users.noreply.github.com>
2024-07-19 16:03:00 -0700

    GCG toxic language attack during PasswordMatch experiment (#633)

---
+++ experiments/oskar/oskar_006_gcg_pm_toxic.py
@@ -0,0 +1,55 @@
+import os
+
+from robust_llm.batch_job_utils import run_multiple
+
+EXPERIMENT_NAME = os.path.basename(__file__).replace(".py", "")
+HYDRA_CONFIG = "oskar/toxic-password-match"
+
+MODELS = [
+    "Qwen/Qwen1.5-0.5B-Chat",
+    "Qwen/Qwen1.5-1.8B-Chat",
+    "Qwen/Qwen1.5-4B-Chat",
+    "Qwen/Qwen1.5-7B-Chat",
+    "Qwen/Qwen1.5-14B-Chat",
+    "meta-llama/Llama-2-7b-chat-hf",
+    "meta-llama/Llama-2-13b-chat-hf",
+]
+
+
+def extract_num_params(model: str):
+    model = model.lower()
+    model = model.replace("-chat", "")
+    model = model.replace("-instruct", "")
+    model = model.replace("-hf", "")
+    num_str = model.split("-")[-1]
+    if num_str.endswith("b"):
+        return int(float(num_str[:-1]) * 1e9)
+    elif num_str.endswith("m"):
+        return int(float(num_str[:-1]) * 1e6)
+    else:
+        raise ValueError(f"Unknown model size: {model}")
+
+
+OVERRIDE_ARGS_LIST = [
+    {
+        "+model": model,
+    }
+    for model in MODELS
+]
+GPU = [max(int(extract_num_params(model) // 3.5e9), 1) for model in MODELS]
+MEMORY = [
+    "{:.0f}G".format(40 + extract_num_params(model) // 0.25e9) for model in MODELS
+]
+CPU = [4 + int(extract_num_params(model) // 3e9) for model in MODELS]
+
+
+if __name__ == "__main__":
+    run_multiple(
+        EXPERIMENT_NAME,
+        HYDRA_CONFIG,
+        OVERRIDE_ARGS_LIST,
+        memory=MEMORY,
+        cpu=CPU,
+        gpu=GPU,
+        priority="normal-batch",
+    )
