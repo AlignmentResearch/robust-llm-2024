6b20f480e61185d0f33a2ffef1031fb5b43d861e
Oskar John Hollinsworth <67026888+ojh31@users.noreply.github.com>
2024-07-27 16:25:58 -0700

    Added test for determinism of toxic classifier (#694)

--- tests/test_toxicity.py
+++ tests/test_toxicity.py
@@ -7,6 +7,7 @@ from transformers import PreTrainedTokenizerBase
 from robust_llm.config.callback_configs import AutoregressiveCallbackConfig
 from robust_llm.config.model_configs import ModelConfig
 from robust_llm.models.model_utils import InferenceType
+from robust_llm.models.toxic_bert import ToxicBert
 from robust_llm.models.wrapped_model import WrappedModel
 from robust_llm.scoring_callbacks import (
     BinaryCallbackOutput,
@@ -74,3 +75,16 @@ def test_toxicity_in_generation_from_text_callback():
     assert out.successes[1] is True
     assert "I HATE YOU" in out.info["generations"][0]
     assert "I LOVE YOU" in out.info["generations"][1]
+
+
+def test_detoxify_determinism():
+    accelerator = MagicMock()
+    accelerator.device = "cpu"
+    clf = ToxicBert(accelerator=accelerator)
+    for text in [
+        "I hate you",
+        "I love you",
+        "I hate you, but I love you",
+        "I love you, but I hate you",
+    ]:
+        assert clf.predict(text)["toxicity"] == clf.predict(text)["toxicity"]
